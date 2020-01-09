01008771a89920f33adcc9f93023c82dc2de6634
Chie <doutorking@gmail.com>
2020-01-09 14:12:01 +0900

    Add test code.

--- src/main/java/com/swabber/controller/HelloController.java
+++ src/main/java/com/swabber/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.swabber.controller;
 
 import com.swabber.service.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
