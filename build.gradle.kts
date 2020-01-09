5d0358341d586c63a51475dbe160207d2467ca96
Chie <doutorking@gmail.com>
2020-01-09 14:01:50 +0900

    Add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id ("org.springframework.boot") version "2.2.2.RELEASE"
+    id ("io.spring.dependency-management") version "1.0.8.RELEASE"
+}
+
+apply(from = "versions.gradle.kts")
+
+group = "com.swabber"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_13
+    targetCompatibility = JavaVersion.VERSION_13
+}
