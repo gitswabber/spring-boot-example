436a65dcac809b9478a96d1fa541be0341b6be13
swabber <doutorking@gmail.com>
2020-01-13 13:23:01 +0900

    Add testcase for UserMapper.

---
+++ src/test/java/com/swabber/repository/UserMapperTest.java
@@ -0,0 +1,36 @@
+package com.swabber.repository;
+
+import com.swabber.controller.dto.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    private UserEntity userEntity = new UserEntity();
+
+    @BeforeEach
+    void setUp() {
+        userEntity.setId(11);
+        userEntity.setFirstName("testFirstName");
+        userEntity.setLastName("testLastName");
+        userEntity.setAge(22);
+    }
+
+    @Test
+    void userEntityToUser() {
+        final User user = userMapper.userEntityToUser(userEntity);
+
+        Assertions.assertEquals(user.getFirstName(), userEntity.getFirstName());
+        Assertions.assertEquals(user.getLastName(), userEntity.getLastName());
+        Assertions.assertEquals(user.getAge(), userEntity.getAge());
+    }
+}
