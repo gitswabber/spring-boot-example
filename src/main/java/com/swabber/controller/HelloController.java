acf23e25a7bab4e8d823e3bd73aaf72d5e6f88b6
swabber <doutorking@gmail.com>
2022-03-06 22:07:13 +0900

    Add endpoints on HelloController

--- src/main/java/com/swabber/controller/HelloController.java
+++ src/main/java/com/swabber/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.swabber.controller;
 
+import com.swabber.controller.dto.Message;
 import com.swabber.service.HelloService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HelloController {
 
@@ -15,7 +20,22 @@ public class HelloController {
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return helloService.getHelloMessage();
+    public ResponseEntity<?> hello() {
+        return new ResponseEntity<>(helloService.getHelloMessage(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public ResponseEntity<?> createMessage(String message) {
+        return new ResponseEntity<>(new Message(message), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.PATCH)
+    public ResponseEntity<?> updateMessage(String message) {
+        return new ResponseEntity<>(new Message(message), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteMessage(String message) {
+        return new ResponseEntity<>(new Message(message), HttpStatus.OK);
     }
 }
